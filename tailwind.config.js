17bc165b6327499b0469e556e8d1d7404d4bccdd
mohmmad taher <76772786+mohmmad-ta@users.noreply.github.com>
2024-11-05 13:18:19 +0300

    start work

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: {
+          950: 'rgba(35,31,32,1)',
+          500: 'rgba(35,31,32,0.5)',
+          300: 'rgba(35,31,32,0.3)',
+          200: 'rgba(35,31,32,0.2)',
+          100: 'rgba(35,31,32,0.1)',
+          50: 'rgba(35,31,32,0.05)',
+        },
+        secondary: {
+          950: 'rgb(113,113,113)',
+          500: 'rgba(113,113,113,0.5)',
+          300: 'rgba(113,113,113,0.3)',
+          200: 'rgba(113,113,113,0.2)',
+          100: 'rgba(113,113,113,0.1)',
+          50: 'rgba(113,113,113,0.05)',
+        },
+        inputColor: {
+          950: 'rgb(53,50,46)',
+        },
+        main: {
+          50: 'rgba(241,90,41,1)',
+        }
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+  },
+  plugins: [],
+}
